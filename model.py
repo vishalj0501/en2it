ab110ad50ebd57beaf5261141095a630dea7e212
vishalj0501 <jvishal0501@gmail.com>
2023-08-20 20:59:32 +0530

    add layer norm & multi-head attn

--- model.py
+++ model.py
@@ -32,3 +32,56 @@ class PositionalEncoding(nn.Module):
     def forward(self,x):
         x=x+torch.tensor(self.pe[:,:x.size(1)],requires_grad=False)
         return self.dropout(x)
+    
+
+class LayerNormalization(nn.Module):
+    def __init__(self,eps: float=10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #x 
+        self.beta = nn.Parameter(torch.zeros(1)) #+
+
+    def forward(self,x):
+        mean = x.mean(-1,keepdim=True)
+        std = x.std(-1,keepdim=True)
+        return self.alpha*(x-mean)/(std+self.eps)+self.beta 
+    
+class FeedForward(nn.Module):
+    def __init__(self,d_model,d_ff,dropout):
+        super().__init__()
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.dropout = nn.Dropout(dropout)
+        self.linear1 = nn.Linear(d_model,d_ff) #w1 & b1
+        self.linear2 = nn.Linear(d_ff,d_model) #w2 & b2 
+        self.relu = nn.ReLU()
+    def forward(self,x):
+        x = self.relu(self.linear1(x))
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_model : int, h : int, dropout : float = 0.1):
+        super().__init__()
+        assert d_model % h == 0, "d_model % h != 0"
+        self.d_model = d_model
+        self.h = h
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model,d_model) #w_q
+        self.w_k = nn.Linear(d_model,d_model) #w_k
+        self.w_v = nn.Linear(d_model,d_model) #w_v
+        self.w_o= nn.Linear(d_model,d_model) #w_o
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self,q,k,v,mask=None):
+        query = self.w_q(q) #
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0],query.shape[1],self.h,self.d_k).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h,self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+        
+        # here, view is used to split the d_model into h heads, and transpose is used to swap the seq_len and head dimensions
+
