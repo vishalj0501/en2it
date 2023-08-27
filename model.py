2ba2030d8e897408c46fec1976d3cb51da342c66
vishalj0501 <jvishal0501@gmail.com>
2023-08-27 18:33:41 +0400

    Add Decoder

--- model.py
+++ model.py
@@ -142,7 +142,38 @@ class Encoder(nn.Module):
      
 
 
+class DecoderBlock(nn.Module):
+    def __init__(self,self_attention_block: MultiHeadAttention, cross_attention_block:MultiHeadAttention,feed_forward:FeedForward,dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for i in range(3)])
 
+    def forward(self,x,enc_output,source_mask,target_mask):
+        x = self.residual_connection[0](x,lambda x: self.self_attention_block(x,x,x,target_mask))
+        x = self.residual_connection[1](x,lambda x: self.cross_attention_block(x,enc_output,enc_output,source_mask))
+        return self.residual_connection[2](x,lambda x: self.feed_forward_block(x))
+    
 
+class Decoder(nn.Module):
+    def __init__(self,layers:nn.ModuleList)->None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+    
+    def forward(self,x,enc_output,source_mask,target_mask):
+        for layer in self.layers:
+            x = layer(x,enc_output,source_mask,target_mask)
+        return self.norm(x)
+    
 
-
+class ProjectionLayer(nn.Module):
+    def __init__(self,d_model: int,vocab_size: int)->None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.proj = nn.Linear(d_model,vocab_size)
+    
+    def forward(self,x):
+        return torch.log_softmax(self.proj(x),dim=-1)
